60af9adca8c6c60f2de531739f6c2a0fde5658fe
Ridzkyan <raihan020705@gmail.com>
2026-05-10 17:03:40 +0800

    anjay

--- auto_gform.py
+++ auto_gform.py
@@ -22,6 +22,7 @@ def fill_form(name):
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--window-size=1920,1080') # Wajib: Atur ukuran layar virtual agar elemen tidak tertutup
+    options.add_argument('--lang=id-ID') # WAJIB: Memaksa Google Form berbahasa Indonesia
     driver = webdriver.Chrome(options=options)
     
     wait = WebDriverWait(driver, 10)
