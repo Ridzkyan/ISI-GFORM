13a0b13878292313a4f916636908ccee5a643b55
Ridzkyan <raihan020705@gmail.com>
2026-05-10 16:53:55 +0800

    anjay

--- auto_gform.py
+++ auto_gform.py
@@ -21,6 +21,7 @@ def fill_form(name):
     options.add_argument('--headless') # Wajib diaktifkan untuk deployment di server
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080') # Wajib: Atur ukuran layar virtual agar elemen tidak tertutup
     driver = webdriver.Chrome(options=options)
     
     wait = WebDriverWait(driver, 10)
